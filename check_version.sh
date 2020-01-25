7259e2fec29b3ce950dde291de6da544fab21d0e
Jon Jagger <jon@jaggersoft.com>
2020-01-25 11:04:27 +0000

    Update check-version script

--- check_version.sh
+++ check_version.sh
@@ -2,7 +2,7 @@
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED=$(cat ${MY_DIR}/README.md | grep Version | cut -d'=' -f2 | cut -d']' -f1)
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/bcpl sh -c '/distribution/BCPL/cintcode/bin/cintsys64 -v')
+readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/bcpl sh -c '/distribution/BCPL/cintcode/bin/cintsys -v')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
