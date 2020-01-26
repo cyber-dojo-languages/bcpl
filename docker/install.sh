3a2e70a2713657b3a16536a3c842031382411494
Jon Jagger <jon@jaggersoft.com>
2020-01-26 09:06:55 +0000

    Try fix for untar permissions

--- docker/install.sh
+++ docker/install.sh
@@ -1,23 +1,29 @@
 #!/bin/sh -Eeu
 
-# install add-apt-repository
+# Instructions from http://www.cl.cam.ac.uk/users/mr/bcplman.pdf Ch12
+# Had to also
+#  o) install open-gl
+#  o) tweak tar; add umask;  and --no-same-permissions
+
+# Install add-apt-repository
 apt-get install --yes software-properties-common
-# use it to get new package source
+# Use it to get new package source
 add-apt-repository universe
 apt update
-# now install open-gl
+# Install open-gl
 apt-get install --yes mesa-utils freeglut3-dev
 
 apt-get install --yes wget
-
 mkdir /distribution
 cd /distribution
 wget http://www.cl.cam.ac.uk/~mr10/BCPL/bcpl.tgz
-tar xvf bcpl.tgz
+umask 022
+tar zxvf bcpl.tgz --no-same-permissions
 cd BCPL/cintcode
 . os/linux/setbcplenv
 make clean
 make sys
-rm /distribution/bcpl.tgz
 
+# Remove unneeded files
+rm /distribution/bcpl.tgz
 apt-get remove --yes wget
